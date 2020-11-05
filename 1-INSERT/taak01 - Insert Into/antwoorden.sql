7efa32843b3fe9ba8aa5eeaab3d8066bb271186e
dardan0118 <22111704@talnet.nl>
2020-11-05 12:57:57 +0100

    mySql insert into

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,6 @@
-
+INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) VALUES ('1','henk','van. balen','9A','albert cypstraat 1','1234AB','2000-05-18','amsterdam')
+INSERT INTO `studenten` (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) VALUES
+('6', 'dardan', 'mustafa', '9Z', 'papmpus laan', '1304rt', '2001-12-22', 'den haag'),
+('7', 'tabie', ' nemok ', '8D', 'spijkenisse', '1129du', '2004-04-13', 'rotterdam'),
+('8', 'qija', 'magbie', '9R', 'weesperstraat', '1254AZ', '2004-01-27', 'denhaag'),
+('9', 'dagie', 'sahbie', '9R', 'kebastraat', '2423RT', '2001-01-17', 'den helder');
